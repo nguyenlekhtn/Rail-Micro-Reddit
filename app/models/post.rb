2aa26dd2e7f37149679c3d834cea38a32da7ebb0
nguyenlekhtn <nguyenlekhtn@gmail.com>
2022-03-24 21:43:31 +0700

    create comment model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   validates :title, presence: true
   validates :link, presence: true, 
     format: { with: /\Ahttps?:\/\//, message: "should have correct format" }
